fbdda0eb069dcd44e1c62f8b2b01ac37ea32ea0f
root <sdickma@purdue.edu>
2019-02-12 18:02:32 -0800

    updated PTE

--- etc/PTE.c
+++ etc/PTE.c
@@ -18,12 +18,33 @@ struct SensorInfo {
 }
 
 struct SensorInfo IMU[] = {
-	"IMU", DEVICE_LOCATION_MB, accel, 0, NULL, 0, NULL, NULL, 0 };
+	"IMU", DEVICE_LOCATION_MB, accel, 0, NULL, 0, NULL, NULL, 0 }; //DEVICE_LOCATION_MB just a filler
+
+struct IMUData {
+	int32_t x[], y[], z[];
+}
 
 //functions
-void PTE_status(int socket, unsigned char cmd, void * data, size_t dataLen, struct sockaddr_in * src)
+static void marshal_accel(struct SensorInfo *si, void *dst)
 {
-	PROC_cmd_sockaddr(proc, CMD_STATUS_RESPONSE; &status, sizeof(status), src);
+	struct AccelerometerSensor *accel = (struct AccelerometerSensor*)si->sensor;
+	struct IMUData *ad = (struct IMUData*)dst;
+	AccelData data;
+
+	if (accel->read) {
+		accel->read(accel, &data);
+		ad->x = hton1(data.x_result);
+		ad->y = hton1(data.y_result);
+		ad->z = hton1(data.z_result);
+	}
+} //note that this is also a placeholder based off ADCS example and needs to be customized
+
+void PTE_run(int socket, unsigned char cmd, void * data, size_t dataLen, struct sockaddr_in * src)
+{
+	//actual PTE goes here
+	//filter then PTE
+
+	PROC_cmd_sockaddr(proc, CMD_STATUS_RESPONSE, &status, sizeof(status), src);
 }
 
 int sigint_handler(int signum, void *arg)
@@ -35,19 +56,15 @@ int sigint_handler(int signum, void *arg)
 int main(int argc, char *argv[])
 {
 	//initialize process
-	proc = PROC_init("PTE");
+	proc = PROC_init("PTE", WD_DISABLE); //watchdog or no?
 	PROC_signal(proc, SIGINT, &sigint_handler, proc);
 
 	//initialize IMU
 
-	//filter data
-
-	//run PTE
-
+	EVT_start_loop(PROC_event(proc));	
 	//set next IMU run time
 
-	//package status
-
 	//cleanup
+	PROC_cleanup(proc);
 	return 0;
 }
