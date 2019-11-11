d5f3477c1a335a3e72f35072039181ba3b673041
Sammy Dickmann <sdickma@purdue.edu>
2019-11-10 21:54:16 -0500

    Testing Commands

--- etc/PTE.c
+++ etc/PTE.c
@@ -482,7 +482,6 @@ void IMU_trigger(int socket, unsigned char cmd, void *data, size_t dataLen, stru
 		// import data
 		point = (double*)data;
 		resp = *point + 1;
-		printf("%lf\n", 5);
 	
 		// run PTE using IMU data
 		//PTE_control(accel_data, mode);
