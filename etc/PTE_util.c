9d856fdc4ae49c975aab4a4cc50c07b7a72a54f9
Sammy Dickmann <sdickma@purdue.edu>
2019-11-17 17:41:00 -0500

    Debugging commands

--- etc/PTE_util.c
+++ etc/PTE_util.c
@@ -81,6 +81,8 @@ static int IMU_trigger(int argc, char **argv, struct MulticallInfo * self)
       uint8_t cmd;
 	  struct IMUData {
 			double t[MAX_PASS];
+			double x[MAX_PASS];
+			double y[MAX_PASS];
 		} send_data;
    } __attribute__((packed)) send;
 
